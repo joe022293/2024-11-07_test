36d87e7bc7f5a107c2de52f3f53604f55c2f746c
joe022293 <139582654+joe022293@users.noreply.github.com>
2024-11-07 19:12:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from reportlab.pdfgen import canvas
-print('1')
+# from reportlab.pdfgen import canvas
+# print('1')
 from PIL import Image
 print('2')
-import qrcode
-print('3')
-from PyPDF2 import PdfFileMerger
-print('4')
-import streamlit as st
-print('5')
-import base64
-print('6')
-import os
-print('succes')
+# import qrcode
+# print('3')
+# from PyPDF2 import PdfFileMerger
+# print('4')
+# import streamlit as st
+# print('5')
+# import base64
+# print('6')
+# import os
+# print('succes')
